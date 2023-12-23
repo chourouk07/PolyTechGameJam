164dd5faacff31e807ef9b8f4c662fe285f4b670
chourouk07 <117457141+chourouk07@users.noreply.github.com>
2023-12-23 08:46:44 +0100

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 1.0f;
+    [SerializeField] private float maxSpeed = 10f;
     [SerializeField] private Rigidbody2D playerRb;
 
     private void Update()
@@ -16,6 +17,27 @@ public class PlayerController : MonoBehaviour
         playerRb.velocity = (moveDirection * playerSpeed);
     }
 
+    public void IncreasePlayerSpeed(float newSpeed)
+    {
+        playerSpeed += newSpeed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacles"))
+        {
+            Destroy(gameObject,0.5f);
+        }
+    }
+    #region getters and setters
+    public float GetSpeed()
+    {
+        return playerSpeed;
+    }
+    public float GetMaxSpeed()
+    {
+        return maxSpeed;
+    }
+    #endregion
 
 
 
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,33 +9,34 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject sideObsPrefab;
     [SerializeField] private GameObject randomObsPrefab;
     #endregion
-    #region Spawn Positions
+    #region Transforms
     [SerializeField] private Transform initDownpos;
     [SerializeField] private Transform initSideObs;
     [SerializeField] private Transform[] randomObsSpawnPts;
     #endregion
-
-    #region
-    public int obstacleCountDown= 4;
-    public int obstacleCountSide= 0;
+    #region Obstacles Number
+    [SerializeField] private int obstacleCountDown = 4;
+    [SerializeField] private int obstacleCountSide = 0;
     #endregion
+    #region 
+    public float spawnDelay = 8f;
+    private Coroutine spawnCoroutine; 
+    #endregion
+    
 
 
-    void Start()
+    private void Start()
     {
-        // Call methods to spawn obstacles based on your game logic
-        //SpawnDownwardObstacles();
-        SpawnHorizontalObstacles();
-        //SpawnRandomObstacles();
+        spawnCoroutine = StartCoroutine(SpawnDelay(spawnDelay));
     }
 
     void SpawnDownwardObstacles()
     {
         float xSpawnPosition = initDownpos.position.x;
-        for (int i = 0; i <= obstacleCountDown; i++)
-        {    
+        for (int i = 0; i < obstacleCountDown; i++)
+        {
             Vector2 spawnpos = new Vector2(xSpawnPosition, initDownpos.position.y);
-            Instantiate(downwardObsPrefab,spawnpos, Quaternion.identity);
+            Instantiate(downwardObsPrefab, spawnpos, Quaternion.identity);
             xSpawnPosition += 4f;
         }
     }
@@ -43,22 +44,80 @@ public class SpawnManager : MonoBehaviour
     void SpawnHorizontalObstacles()
     {
         float ySpawnPosition = initSideObs.position.y;
-        for (int i = 0; i <= obstacleCountSide; i++)
+        for (int i = 0; i < obstacleCountSide; i++)
         {
-            Vector2 spawnpos = new Vector2(initSideObs.position.x,ySpawnPosition);
-            GameObject obstacle = Instantiate(sideObsPrefab,spawnpos, Quaternion.identity);
+            Vector2 spawnpos = new Vector2(initSideObs.position.x, ySpawnPosition);
+            Instantiate(sideObsPrefab, spawnpos, Quaternion.identity);
             ySpawnPosition -= 4f;
         }
     }
 
     void SpawnRandomObstacles()
     {
-        
-        /*for (int i = 0; i <= randomObsSpawnPts.Length; i++)
+
+        for (int i = 0; i < randomObsSpawnPts.Length; i++)
         {
-            Vector2 spawnpos = new Vector2(randomObsSpawnPts[i].position.x, randomObsSpawnPts[i].position.y );
-            GameObject obstacle = Instantiate(ran, spawnpos, Quaternion.identity);
-            ySpawnPosition -= 4f;
-        }*/
+            Vector2 spawnpos = new Vector2(randomObsSpawnPts[i].position.x, randomObsSpawnPts[i].position.y);
+            Instantiate(randomObsPrefab, spawnpos, Quaternion.identity);
+        }
     }
+
+    IEnumerator SpawnDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            SpawnObstacles();
+        }
+    }
+
+    void SpawnObstacles()
+    {
+        int previousIndex = -1;
+        int randomIndex;
+            do
+            {
+                randomIndex = Random.Range(0, 3);
+            } while (randomIndex == previousIndex);
+
+            previousIndex = randomIndex;
+
+            switch (randomIndex)
+            {
+                case 0:
+                    SpawnDownwardObstacles();
+                    break;
+
+                case 1:
+                    SpawnHorizontalObstacles();
+                    break;
+                case 2:
+                    SpawnRandomObstacles();
+                    break;
+                default: break;
+            }
+    }
+
+    public void IncreaseSpawnDelay(float newDelay)
+    {
+        // Stop the existing coroutine
+        
+
+        // Increase the spawn delay
+        spawnDelay += newDelay;
+
+        // Start a new coroutine with the updated delay
+        
+        if (spawnCoroutine != null)
+        {
+            StopCoroutine(spawnCoroutine);
+            spawnCoroutine = StartCoroutine(SpawnDelay(spawnDelay));
+        }
+    }
+
+    public float GetSpawnDelay()
+    {
+        return spawnDelay;
+    }
+
 }
